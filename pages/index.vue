bafdb9a4502bcbac14d416989212042781438850
Amon Sawamura <amon.sawamura@gmail.com>
2021-01-05 22:31:03 +0900

    add pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>Hello world!</h1>
+    <p v-for="post in posts" :key="post.id">
+      <NuxtLink :to="`/posts/${post.id}`">{{ post.title }}</NuxtLink>
+    </p>
+  </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $axios, $config }) {
+    const posts = await $axios.$get('/posts?fields=id,title', {
+      headers: {
+        'X-API-KEY': $config.microcms.apiKey,
+        'X-GLOBAL-DRAFT-KEY': $config.microcms.globalDraftKey,
+      },
+    });
+    return {
+      posts: posts.contents,
+    };
+  },
+};
+</script>
---
+++ pages/posts/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ body }}</p>
+    <p><NuxtLink to="/">トップに戻る</NuxtLink></p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $axios, $config }) {
+    const post = await $axios.$get(`/posts/${params.id}?fields=title,body`, {
+      headers: {
+        'X-API-KEY': $config.microcms.apiKey,
+        'X-GLOBAL-DRAFT-KEY': $config.microcms.globalDraftKey,
+      },
+    });
+    return post;
+  },
+};
+</script>
