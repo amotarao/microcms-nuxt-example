2f5993825feebfb6853a634f38253862e914f10b
Amon Sawamura <amon.sawamura@gmail.com>
2021-01-05 22:31:00 +0900

    add microcms config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,18 @@
+const microcms = {
+  apiKey: process.env.MICROCMS_API_KEY || '', // X-API-KEY
+  globalDraftKey: process.env.MICROCMS_GLOBAL_DRAFT_KEY || '', // X-GLOBAL-DRAFT-KEY
+};
+
 export default {
   target: 'static',
   modules: ['@nuxtjs/axios'],
+  publicRuntimeConfig: {
+    microcms: process.env.NODE_ENV === 'production' ? {} : microcms,
+  },
+  privateRuntimeConfig: {
+    microcms,
+  },
+  axios: {
+    baseURL: `https://${process.env.MICROCMS_SERVICE_NAME}.microcms.io/api/v1`,
+  },
 };
